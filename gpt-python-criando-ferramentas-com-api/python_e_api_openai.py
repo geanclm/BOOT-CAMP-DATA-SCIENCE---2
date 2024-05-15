50eae85fdd523cfb409defc36d4b047d0f1371ec
Gean Carlos <geanclm@gmail.com>
2024-05-15 12:11:45 -0300

    Python API OpenAI
    início do curso

---
+++ gpt-python-criando-ferramentas-com-api/python_e_api_openai.py
@@ -0,0 +1,14 @@
+# GPT e Python: criando ferramentas com a API da OpenAI
+# Curso e treinamento Plataforma ALura - período de teste
+# by geanclm on 15/05/2024
+
+# ETAPA 1
+# Recomendação profissional para todo trabalho de código
+# Criando e ativando o ambiente virtual
+# python -m venv curso_1_openai
+# curso_1_openai\Scripts\activate
+
+# ETAPA 2
+# Instalação das bibliotecas
+# pip install openai python-dotenv pydub instabot Pillow
+
